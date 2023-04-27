ea4713d9cabc3d191b2a56397615ce7a0061718f
Hiago <hiagosmd@hotmail.com>
2023-04-27 18:51:44 -0300

    Implemented solution for Challenge 1 with smooth image loading

--- miniBootcampChallenge/ImageCell.swift
+++ miniBootcampChallenge/ImageCell.swift
@@ -9,12 +9,29 @@ class ImageCell: UICollectionViewCell {
     
     @IBOutlet weak var imageView: UIImageView!
     
+    // Prepare the cell for reuse
     override func prepareForReuse() {
         super.prepareForReuse()
         imageView.image = nil
     }
     
+    // Display the image
     func display(_ image: UIImage?) {
         imageView.image = image
     }
+
+    // Configure the cell with the image URL for asynchronous downloading
+    func configure(with imageURL: URL) {
+        // Create a URLSession data task to download the image
+        URLSession.shared.dataTask(with: imageURL) { data, _, _ in
+            if let data = data, let image = UIImage(data: data) {
+                // Update the UI on the main thread
+                DispatchQueue.main.async {
+                    self.display(image)
+                }
+            }
+        }.resume() // Start the data task
+    }
 }
+
+
--- miniBootcampChallenge/ViewController.swift
+++ miniBootcampChallenge/ViewController.swift
@@ -3,6 +3,18 @@
 //  miniBootcampChallenge
 //
 
+// TODO: 1.- Implement a function that allows the app downloading the images without freezing the UI or causing it to work unexpected way
+
+// Solution for TODO 1:
+// Implemented an asynchronous image downloading method inside the ImageCell class.
+// This method is called in the cellForItemAt method, which prevents the UI from freezing
+// and allows smooth scrolling in the UICollectionView.
+
+
+
+
+// TODO: 2.- Implement a function that allows to fill the collection view only when all photos have been downloaded, adding an animation for waiting the completion of the task.
+
 import UIKit
 
 class ViewController: UICollectionViewController {
@@ -21,15 +33,7 @@ class ViewController: UICollectionViewController {
         super.viewDidLoad()
         title = Constants.title
     }
-
-
 }
-
-
-// TODO: 1.- Implement a function that allows the app downloading the images without freezing the UI or causing it to work unexpected way
-
-// TODO: 2.- Implement a function that allows to fill the collection view only when all photos have been downloaded, adding an animation for waiting the completion of the task.
-
 
 // MARK: - UICollectionView DataSource, Delegate
 extension ViewController {
@@ -37,24 +41,22 @@ extension ViewController {
         urls.count
     }
     
+    // Updated cellForItemAt method to use the configure(with:) method for asynchronous image downloading
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.cellID, for: indexPath) as? ImageCell else { return UICollectionViewCell() }
         
         let url = urls[indexPath.row]
-        let data = try? Data(contentsOf: url)
-        let image = UIImage(data: data!)
-        cell.display(image)
+        cell.configure(with: url) // Call the configure method with the image URL
         
         return cell
     }
 }
 
-
 // MARK: - UICollectionView FlowLayout
 extension ViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         if Constants.cellSize == nil {
-          let layout = collectionViewLayout as! UICollectionViewFlowLayout
+            let layout = collectionViewLayout as! UICollectionViewFlowLayout
             let emptySpace = layout.sectionInset.left + layout.sectionInset.right + (Constants.columns * Constants.cellSpacing - 1)
             Constants.cellSize = (view.frame.size.width - emptySpace) / Constants.columns
         }
